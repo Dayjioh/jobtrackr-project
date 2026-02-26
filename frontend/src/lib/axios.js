41726012369358773d55ba4b12919a9100c4961b
Dayjioh <djo.debbou@gmail.com>
2026-02-26 23:55:35 +0100

    🔨 Frontend : axios response interceptor

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -5,4 +5,15 @@ const API = axios.create({
   withCredentials: true, // send cookies to the server
 });
 
+// Add a response interceptor
+axios.interceptors.response.use(function onFulfilled(response) {
+    // Any status code that lie within the range of 2xx cause this function to trigger
+    // Do something with response data
+    return response;
+  }, function onRejected(error) {
+    // Any status codes that falls outside the range of 2xx cause this function to trigger
+    // Do something with response error
+    return Promise.reject(error);
+  });
+
 export default API;
